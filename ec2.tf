8dc4a16568ce0fcc5f2a861e7422f14b6d800aea
mary obi <maryobit@gmail.com>
2022-11-18 14:41:29 -0600

    fix codes monitoring spelling

--- ec2.tf
+++ ec2.tf
@@ -1,7 +1,7 @@
 resource "aws_instance" "web" {
   ami           = var.AMI_ID
   instance_type = var.INSTANCE_TYPE
-  moitoring = true
+  monitoring = true
   ebs_optimized = true
   tags = {
     Name = "airbnb-web-server"
